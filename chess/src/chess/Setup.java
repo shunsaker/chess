d5ffe9350b5cfc20fc0b589e2fb6f58ed88f46b9
shunsaker <shunsaker@student.neumont.edu>
2014-05-15 16:04:25 -0300

    Improved module 1

--- chess/src/chess/Setup.java
+++ chess/src/chess/Setup.java
@@ -3,17 +3,30 @@ package chess;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Setup {
 	// board must be 8 x 8 for this regex to work
 	private static final String pattern = "(?i)([bkqnrp][ld][a-h][1-8])|([a-h][1-8] [a-h][1-8]\\*?( [a-h][1-8] [a-h][1-8])?)";
 	public static final Scanner SCAN = new Scanner(System.in);
+	private static final Map<Character, String> PIECE_MAP = new HashMap<Character, String>();
+	static {
+		PIECE_MAP.put('r', "Rook");
+		PIECE_MAP.put('n', "Knight");
+		PIECE_MAP.put('q', "Queen");
+		PIECE_MAP.put('b', "Bishop");
+		PIECE_MAP.put('p', "Pawn");
+		PIECE_MAP.put('k', "King");
+	}
 	
 	public static void parseLine(String line) {
 		if(line != null && line.matches(pattern)) {
+			line = line.toLowerCase();
 			if(line.length() == 4) {	
-				System.out.println("Place " + line.charAt(0) + " at " + line.substring(2, 4));
+				String color = (line.substring(1,2).equalsIgnoreCase("l")) ? "White" : "Black";
+				System.out.println("Place " + color + " " + PIECE_MAP.get(line.charAt(0)) + " at " + line.substring(2, 4));
 			}
 			else {
 				System.out.print("Move whatever is at " + line.substring(0, 2) + " to " + line.substring(3,5));
@@ -23,7 +36,6 @@ public class Setup {
 		}
 		else 
 			System.out.println("invalid: " + line);
-		
 	}
 	
 	public static void driver(String fileAddress) {
